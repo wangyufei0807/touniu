8cd9a4104310c7992ded1cab5852ab9c4fa6c54b
wangyufei <wangyufei3360@126.com>
2015-08-30 18:13:59 +0800

    新增内容

--- js/page/index.js
+++ js/page/index.js
@@ -8,12 +8,23 @@
 define(function(require, exports, module){
 
 	require("jquery");
-	require("kinmaxshow");
 
 	//初始化方法
 	function _init(){
 		//$("#kinMaxShow").kinMaxShow();
+		var li = $(".ui-direct").find('.list').find('li');
+		removeMr(li,4);
 	}
+
+	//去除margin-right
+	function removeMr(elem,column){
+		elem.each(function(index, el) {
+			if (($(this).index()+1)% column == 0) {
+				$(this).css("margin-right","0px");
+			};		
+		});
+	}
+
 
 	$(function(){
 		_init();
